122f2e0eaf76f41ca5765e765b4018fded0ad535
Muhammetmyrat <bayramovmuhammetmyrat97@gmail.com>
2024-03-14 14:28:49 +0500

    add:css loaders

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { calc } from './test'
 
 console.log('Hello world')
-console.log(calc(4, 4))
+console.log(calc(4, 20))
--- webpack.config.ts
+++ webpack.config.ts
@@ -2,6 +2,7 @@ import path from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 import 'webpack-dev-server'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import type { Configuration as DevServerConfiguration } from 'webpack-dev-server'
 
 type Mode = 'development' | 'production'
@@ -10,11 +11,25 @@ interface Environment {
   mode: Mode
 }
 export default (env: Environment) => {
+  const isDev = env.mode === 'development'
+
   const config: webpack.Configuration = {
     mode: env.mode ?? 'development',
     entry: path.resolve(__dirname, 'src', 'index.ts'),
     module: {
       rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            ,
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
+        },
         {
           test: /\.tsx?$/,
           use: 'ts-loader',
@@ -30,11 +45,18 @@ export default (env: Environment) => {
       filename: '[name].[contenthash].js',
       clean: true,
     },
-    plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') })],
-    devServer: {
-      port: 5000,
-      open: true,
-    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
+      isDev && new webpack.ProgressPlugin(),
+      !isDev && new MiniCssExtractPlugin({ filename: 'css/[name].[contenthash:8].css', chunkFilename: '[name].[contenthash:8].css' }),
+    ].filter(Boolean),
+    devtool: isDev && 'inline-source-map',
+    devServer: isDev
+      ? {
+          port: 5000,
+          open: true,
+        }
+      : undefined,
   }
 
   return config
